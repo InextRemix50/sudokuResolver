1e20a9eabc467fc011e812e6e300769a8c9df59f
InextRemix50 <remi.alonso@gmail.com>
2015-03-11 09:17:26 +0100

    Message

---
+++ sudokuResolver/src/main/java/com/inextremix/corp/sudokuResolver/App.java
@@ -0,0 +1,13 @@
+package com.inextremix.corp.sudokuResolver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
